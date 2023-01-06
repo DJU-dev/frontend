09355674b99be754f05b5b79d8dce60fb109eb3c
dawon <tomatomato2121@gmail.com>
2023-01-06 12:44:41 +0900

    edit new cube, add move event

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,33 +1,37 @@
-import styled, {keyframes} from "styled-components";
+import styled from "styled-components";
 import {Mobile, Desktop, Tablet} from "@/components/MediaQuery/MediaQuery.jsx";
+import {useState, useEffect} from "react";
+import PackageCube from 'react-3d-cube';
 
+const DESKTOP_CUBE_SIZE = 6.5
+const TABLET_CUBE_SIZE = 3
+const MOBILE_CUBE_SIZE = 2.5
 
-const DESKTOP_CUBE_SIZE = ['16vw', '8vw']
-const TABLET_CUBE_SIZE = ['30vw', '15vw']
-const MOBILE_CUBE_SIZE = ['40vw', '20vw']
+function getWindowSize() {
+    const {innerWidth, innerHeight} = window;
+    return {innerWidth, innerHeight};
+}
 
 const ResponsiveBox = ({size}) => {
+    const [windowSize, setWindowSize] = useState(getWindowSize());
+
+    useEffect(() => {
+        function handleWindowResize() {
+            setWindowSize(getWindowSize());
+        }
+        console.log(windowSize);
+        window.addEventListener('resize', handleWindowResize);
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
     return (
-        <Box size={size[0]}>
-            <Front className="card" size={size[1]}>
-                <CubeImage src={"https://images.unsplash.com/photo-1672699067334-00ec47152b50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"} />
-            </Front>
-            <Back className="card" size={size[1]}>
-                <CubeImage src={"https://images.unsplash.com/photo-1672741511537-4ccaa5096b4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"} />
-            </Back>
-            <Left className="card" size={size[1]}>
-                <CubeImage src={"https://images.unsplash.com/photo-1672745093214-3fb65f2be5a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"} />
-            </Left>
-            <Right className="card" size={size[1]}>
-                <CubeImage src={"https://images.unsplash.com/photo-1672655134101-25d8277f7901?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2MHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"} />
-            </Right>
-            <Top className="card" size={size[1]}>
-                <CubeImage src={"https://plus.unsplash.com/premium_photo-1669658981960-ad1ef6d51571?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"} />
-            </Top>
-            <Bottom className="card" size={size[1]}>
-                <CubeImage src={"https://images.unsplash.com/photo-1672679434929-3518f74d4a6b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"} />
-            </Bottom>
-        </Box>
+        <div>
+            <BoxContainer size={windowSize.innerWidth / size}>
+                <PackageCube size={windowSize.innerWidth / size} index="front" />
+            </BoxContainer>
+        </div>
     )
 }
 
@@ -57,80 +61,9 @@ const Container = styled.div`
     background: black;
 `
 
-const animate = keyframes`
-    0% {
-        transform: rotateX(45deg) rotateY(-45deg);
-    }
-    25% {
-        transform: rotateX(-45deg) rotateY(-45deg);
-    }
-    50% {
-        transform: rotateX(45deg) rotateY(45deg);
-    }
-    75% {
-        transform: rotateX(-45deg) rotateY(45deg);
-    }
-    100% {
-        transform: rotateX(45deg) rotateY(-45deg);
-    }
-`
-
-const Box = styled.div`
+const BoxContainer = styled.div`
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
   margin-top: 200px;
-  height: ${props => props.size || '14vw'};
-  width: ${props => props.size || '14vw'};
-  transform-style: preserve-3d;
-  animation: ${animate} 30s infinite;
-  .card {
-    height: ${props => props.size || '14vw'};
-    width: ${props => props.size || '14vw'};
-    border: 5px solid white;
-    box-sizing: border-box;
-    position: absolute;
-  }
-  .card::after {
-    content: '';
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(315deg, #ffbc00, #ff0058);
-    filter: blur(10px);
-    z-index: -1;
-  }
 `
-
-const CubeImage = styled.img`
-  width: 100%;
-  height: 100%;
-`
-const Front = styled.div`
-    transform: translateZ(${props => props.size || '7vw'});
-`
-
-const Back = styled.div`
-    transform: translateZ(${props => props.size || '-7vw'});
-`
-
-const Left = styled.div`
-    right: ${props => props.size || '7vw'};
-    transform: rotateY(-90deg);
-`
-
-const Right = styled.div`
-    left: ${props => props.size || '7vw'};
-    transform: rotateY(90deg);
-`
-
-const Top = styled.div`
-    bottom: ${props => props.size || '7vw'};
-    transform: rotateX(90deg);
-`
-
-const Bottom = styled.div`
-    top: ${props => props.size || '7vw'};
-    transform: rotateX(-90deg);
-`
-
 export default Cube;
