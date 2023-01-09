24dea26a2612d17726a76ef0fdb981501aaec80c
Chun-Bae <dbtjrdla2056@gmail.com>
2023-01-09 17:17:11 +0900

    Add Grid component success!

---
+++ src/components/Grid.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+function Grid(){
+    return (
+        <div>
+            <a>Grid 추가 성공</a>
+        </div>
+    )
+}
+
+export default Grid;
--- src/screen/Main.jsx
+++ src/screen/Main.jsx
@@ -1,11 +1,14 @@
 import Cube from "@/components/Cube.jsx";
 import Navibar from "@/components/Navibar.jsx";
+import Grid from "@/components/Grid.jsx";
 
 function Main() {
     return (
         <div>
             <Navibar />
+            <Grid />
             <Cube />
+        
         </div>
     )
 }
