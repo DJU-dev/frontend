41ee805bcccea841f3f30657da6f6352fb9a7c21
Chun-Bae <dbtjrdla2056@gmail.com>
2023-01-11 01:43:43 +0900

    create footer frame

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,24 @@
 import styled from "styled-components";
 
 const Container = styled.div`    
-    position: relative;    
+    position: relative;
+    margin-top: 10rem;
+    width: 100%;
+    height: 13rem;
+    border: 1px solid white;
+    border-radius: 2rem 2rem 0 0;
+    background: linear-gradient(40deg,#FF9898,#8054FF);
+`;
+
+const P = styled.p`
+  color: wheat;
+  font-size: 60px; 
 `;
 
 function Footer(){
     return (        
         <Container>
-            <p>Hello World</p>            
+            <P>Hello World</P>            
         </Container>
         
     )
