bbc92076fdc99fa935f4fca0669e6d099b0f2201
dawon <tomatomato2121@gmail.com>
2023-01-11 00:08:51 +0900

    axios

--- src/Login/Register.jsx
+++ src/Login/Register.jsx
@@ -13,6 +13,13 @@ export default function Register() {
 
     const onSubmit = (e) => {
         e.preventDefault();
+        Axios.post("http://127.0.0.1:8000/accounts/registration/", inputs)
+            .then(res => {
+                console.log(res)
+            })
+            .catch(error => {
+                console.log(error)
+            })
         reset();
     }
 
