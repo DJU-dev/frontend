3c8855f1ceb353743e3563bd3a3ec730a61a37bd
Chun-Bae <dbtjrdla2056@gmail.com>
2023-01-14 06:07:29 +0900

    commit cancel restoration

---
+++ src/screen/Test.jsx
@@ -0,0 +1,14 @@
+import Navibar from "@/components/Navibar.jsx";
+import SideBar from "@/components/SideBar.jsx";
+import Board from "@/components/Board.jsx";
+function Test() {    
+    return (        
+        <div>
+            <Navibar />
+            <SideBar />
+            <Board />
+        </div>
+    )
+}
+
+export default Test;
