36c5a83644bf792133507a8bf7f46ff5ea2d593f
tae hyeon <pjbae2007@gmail.com>
2023-01-16 01:26:13 +0900

    Shortcut

--- src/screen/Main.jsx
+++ src/screen/Main.jsx
@@ -1,13 +1,14 @@
 import Cube from "@/components/Cube.jsx";
 import Navibar from "@/components/Navibar.jsx";
 import Grid from "@/components/Grid.jsx";
-
+import ShortCut from "../components/ShortCut";
 function Main() {
     return (
         <div>
-            <Navibar />
+            {/* <Navibar />
             <Grid /> 
-            <Cube />                         
+            <Cube />                          */}
+            <ShortCut/>
         </div>
     )
 }
