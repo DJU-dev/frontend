dd3b92393155dfb3ee92b256154c453ab391a6ad
Chun-Bae <dbtjrdla2056@gmail.com>
2023-01-27 01:06:56 +0900

    just

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import VitePluginHtmlEnv from 'vite-plugin-html-env'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : "/",
   plugins: [react(), VitePluginHtmlEnv()],
   resolve: {
     alias: [
